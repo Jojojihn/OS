fa3dea392b919e4e13628006eb519f3dfd16665b
Jatoxo <39669932+Jatoxo@users.noreply.github.com>
2021-05-12 04:13:10 +0200

    UI Lists: Partial Implementation
    Still needs scroll timing and dynamic scrollbar done

--- include/UILists.h
+++ include/UILists.h
@@ -5,22 +5,30 @@
 
 #include "List.h"
 #include "system/Displays.h"
+#include "system/Input.h"
 
 /**
  * An Item to be displayed in a list
  */
 class UIListItem {
     public: 
+        UIListItem(String text);
+        virtual ~UIListItem();
+
+
         ///The String this item will show up as in the list.
         String listTitle;
-
-        UIListItem(String text);
 
     protected:
         UIListItem();
 
         ///Called when this item becomes highlighted in the list.
         virtual void listOnSelect();
+
+    private:
+        unsigned int scrollIndex;
+
+        friend class UIList;
 };
 
 
@@ -28,18 +36,18 @@ class UIList {
     public:
         ~UIList();
 
+
         /**
          * Creates a new UIList. Use "show" to show the list.
          * 
-         * @param items An array of Pointers to the UIListItems to be used in this list
+         * @param items A Pointer to a List of UIListItems. Make sure this list doesn't go out of scope within the lifetime of this object.
          * 
          * @param size The size of the item array @warning Size must never be bigger than the actual size of the items array, otherwise it will lead to undefined behaviour
          * 
          * @param infinite Whether this list is infinite (Wrapping around)
          * 
-         * @param destroyItems Whether to delete the UIListItems and the array when the list is destroyed
          */
-        UIList(UIListItem *items[], unsigned int size, bool infinite = false, bool destroyItems = true);
+        UIList(List<UIListItem> *items, unsigned int size, bool infinite = false);
 
         /**
          * Creates a new UIList. Use "show" to show the list.
@@ -50,7 +58,17 @@ class UIList {
          * 
          * @param infinite Whether this list is infinite (Wrapping around)
          */
-        UIList(String *items[], unsigned int size, bool infinite = false);
+        UIList(String items[], unsigned int size, bool infinite = false);
+
+
+        /**
+         * Shows the list on the specified display
+         * 
+         * @param display The LcdDisplay on which to show the list
+         * 
+         * @return The index of the object that was selected in the list
+         */
+        int show(LcdDisplay *display);
 
 
         /**
@@ -66,21 +84,56 @@ class UIList {
          */
         static int scrolledList(UIListItem *items[], bool infiniteScroll = false, LcdDisplay *display = Displays::getPrimaryDisplay());
 
+    
+
+
     private:
-        ///Whether to destroy the listitems when the list is destroyed
-        bool destroyItems;
+        /**
+         * Draws a scrollbar in the leftmost column of the specified LcdDisplay. The scroll handle will 
+         * adjust it's size and position to represent the scroll state. This will use the first four custom
+         * character slots on the LCD.
+         * 
+         * @param display The display to show the scrollbar on. 
+         * 
+         * @param totalCount The total amount of things this scrollbar is representing
+         * 
+         * @param visibleCount The amount of visible things from the total things (used to determine scroll handle size)
+         * 
+         * @param position The position of the first visible thing relative to the total amount of things
+         * 
+         */
+        void scrollbar(LcdDisplay *display, unsigned int totalCount, unsigned int visibleCount, unsigned int position);
+
+        void drawLine(LcdDisplay *display, UIListItem *item, unsigned int row, unsigned int startIndex, unsigned int endIndex);
+
+        void redraw(LcdDisplay *display);
+
+        void select(LcdDisplay *display, unsigned int selIndex, bool force = false);
+
+        struct ListPos {
+            ///The index of the first item in the list
+            unsigned int index;
+
+            ///The index of the selected item in the list
+            unsigned int selection;
+
+            ListPos();
+        };
+
+        ///Whether to destroy the list when the object gets destructed
+        bool destroyList;
 
         ///The amount of elements in this list
         unsigned int length;
 
         ///All the items in this list (Pointer to array of pointers)
-        UIListItem **items;
+        List<UIListItem> *items;
 
         ///Whether this list is infinite (wraps around)
         bool infinite;
     
-        ///The current position in the list (Index of the first displayed item)
-        unsigned int position;
+        ///The state of the list (Position & Selection)
+        ListPos state;
 
         UIList();
 
--- src/UILists.cpp
+++ src/UILists.cpp
@@ -1,46 +1,177 @@
 #include "UILists.h"
 
 
-UIListItem::UIListItem() {}
-UIListItem::UIListItem(String text) : listTitle(text) {}
+UIListItem::UIListItem(String text) : listTitle(text), scrollIndex(0) {}
+UIListItem::~UIListItem() {}
+UIListItem::UIListItem() : scrollIndex(0) {}
+
 
 
 void UIListItem::listOnSelect() {}
 
 
 
+UIList::ListPos::ListPos() : index(0), selection(0) {}
+
 
 UIList::~UIList() {
-    if(destroyItems) {
-        for(unsigned int i = 0; i < length; i++) {
-            delete items[i];
+    if(destroyList) {
+        items->clear(true);
+        delete items;
+    }
+}
+
+UIList::UIList(List<UIListItem> *items, unsigned int size, bool infinite) :
+    destroyList(false),
+    length(size),
+    items(items),
+    infinite(infinite),
+    state(ListPos()) {}
+
+
+UIList::UIList(String items[], unsigned int size, bool infinite) :
+    destroyList(true), 
+    length(size),
+    infinite(infinite),
+    state(ListPos())
+    {
+        this->items = new List<UIListItem>();
+
+        for(unsigned int i = 0; i < size; i++) {
+            this->items->add(new UIListItem(items[i]));
         }
-        delete[] items;
+}
+
+
+void UIList::scrollbar(LcdDisplay *display, unsigned int totalCount, unsigned int visibleCount, unsigned int position) {
+
+}
+
+//Draws a single line respecting it's scroll position and margins
+//Both start and endIndex are inclusive
+void UIList::drawLine(LcdDisplay *display, UIListItem *item, unsigned int row, unsigned int startIndex, unsigned int endIndex) {
+    
+    startIndex = min(startIndex, endIndex);
+
+    //If there are at least two spaces for text available...
+    if(endIndex - startIndex >= 2) {
+    
+        //Minus one because zero indexed, minus one again because one column needs to be free for the cursor
+        endIndex = min(display->getSize().x-2, endIndex);
+
+        //The amount of space available for the text
+        unsigned int length = endIndex - startIndex;
+
+        String displayedText = item->listTitle.substring(item->scrollIndex);
+        
+        bool oversized = displayedText.length() > length;
+        if(oversized) {
+            displayedText.remove(length-1);
+        }
+        display->setCursor(startIndex, row);
+
+        display->print(displayedText);
+        if(oversized) {
+            byte ellipsis[8];
+            ellipsis [7] = B10101;
+            display->createChar(4, ellipsis);
+            display->write(4);
+        } else {
+            unsigned int emptyCols = endIndex - (startIndex + displayedText.length() -1);
+            
+            for(unsigned int col = 0; col < emptyCols; col++) {
+                display->print(' ');
+            }
+            
+        }
+
+
+
+    }
+
+}
+
+//Draws all the lines again according to the display state and renders the scrollbar
+void UIList::redraw(LcdDisplay *display) {
+    items->startIteration(state.index);
+    for(unsigned int row = 0; row < display->getSize().y; row++) {
+
+        UIListItem *item = items->iterate();      
+        drawLine(display, item, 1, row, display->getSize().x -1 - 3);
+
+    }
+    scrollbar(display, items->size(), display->getSize().y, state.index);
+}
+
+
+//Move the selector and redraw the entries if required
+void UIList::select(LcdDisplay *display, unsigned int selIndex, bool force) {
+    selIndex = min(items->size() - 1, selIndex);
+
+    //If the new index isn't the same as before, we need to update something
+    if(selIndex != state.selection || force) {
+        unsigned int prevSelIndex = state.selection;
+        state.selection = selIndex;
+
+        //The row on the display that the new selection will be on
+        unsigned int selRow = min(display->getSize().y - 1, selIndex - state.index);
+
+        //The index of the slected item minus the row it is on should result in the index of the
+        //  first item visible on the list. If that's not the case, we need to redraw the list
+
+        
+        if((selIndex - selRow) != state.index) {
+            state.index = selIndex - selRow;
+            redraw(display);
+
+        } else {
+            UIListItem *item = items->get(prevSelIndex);
+
+            if(item->scrollIndex != 0) { //If the list hasn't changed, but the previous selection has scrolled text, reset the scroll on that line and redraw it
+                
+                item->scrollIndex = 0;
+                drawLine(display, item, prevSelIndex - state.index, 1, display->getSize().x -1 - 3);
+                
+            }
+
+        }
+            
+        
+        for(unsigned int row = 0; row < display->getSize().y; row++) {
+
+            display->setCursor(display->getSize().x - 1, row);
+            (row == selRow) ? display->print("<") : display->print(" "); 
+
+        }
+            
+        
+
     }
 }
 
 
-UIList::UIList(UIListItem *items[], unsigned int size, bool infinite = false, bool destroyItems = true) :
-    destroyItems(destroyItems),
-    length(size),
-    items(items),
-    infinite(infinite),
-    position(position) {}
+int UIList::show(LcdDisplay *display) {
+    display->clear();
 
 
-UIList::UIList(String *items[], unsigned int size, bool infinite = false) :
-    destroyItems(true), 
-    length(size),
-    infinite(infinite) 
-    {
-        //TODO: AAAAAAAA
-        UIListItem *listItems[size];
 
-        for(unsigned int i = 0; i < size; i++) {
-            listItems[i] = new UIListItem(*items[i]);
+    redraw(display);
+    select(display, state.selection, true);
+
+    
+    while(!Input::isActionJustPressed("back")) {
+        
+        if(Input::isActionJustPressed("down")) {
+            select(display, state.selection + 1);
+
+        } else if(Input::isActionJustPressed("up")) {
+            select(display, state.selection - 1);
+
+        } else if(Input::isActionJustPressed("confírm")) {
+            return state.selection;
         }
-
-        this->items = ;
-
     }
+    
 
+    return -1;
+}
